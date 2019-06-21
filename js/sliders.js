4fb984d83a4b247a77f67ec54a69309758a9e836
Sergey Vasilyev <kosh.goo@gmail.com>
2019-06-21 10:27:42 +0300

    pure css service slider

--- js/sliders.js
+++ js/sliders.js
@@ -2,11 +2,11 @@ const sliderBtns = document.querySelectorAll('.slider__button');
 const slides = document.querySelectorAll('.slider__item');
 const sliderActiveClass = 'slider__item--active';
 
-const servicesBtnsNodeList = document.querySelectorAll('.services__button');
+/* const servicesBtnsNodeList = document.querySelectorAll('.services__button');
 const serviceBtnsArr = [];
 const servicesSlides = document.querySelectorAll('.services__info');
 const btnActiveClass = 'services__button--active';
-const servicesActiveSlide = 'services__info--active';
+const servicesActiveSlide = 'services__info--active'; */
 
 for (let i = 0; i < sliderBtns.length; i++) {
   sliderBtns[i].addEventListener('click', function (evt) {    
@@ -22,11 +22,11 @@ for (let i = 0; i < sliderBtns.length; i++) {
   })
 }
 
-for (let i = 0; i < servicesBtnsNodeList.length; i++) {
+/* for (let i = 0; i < servicesBtnsNodeList.length; i++) {
   servicesBtnsNodeList[i].addEventListener('click', function () {    
     document.querySelector('.' + btnActiveClass).classList.remove(btnActiveClass);
     this.classList.add(btnActiveClass);
     document.querySelector('.' + servicesActiveSlide).classList.remove(servicesActiveSlide);
     servicesSlides[i].classList.add(servicesActiveSlide);    
   })
-}
+} */
